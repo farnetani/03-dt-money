53378f7e8342e102d8791d95b5afe94e21a60256
Arlei F. Farnetani Junior <farnetani@gmail.com>
2021-04-08 16:35:29 -0300

    MirajgeJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container } from './styles'
 
 export function TransactionsTable() {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }, []) // vai buscar uma única vez
+
   return (
     <Container>
       <table>
